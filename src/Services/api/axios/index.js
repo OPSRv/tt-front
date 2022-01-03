0513ce136980ab7d873ab7bb5b40fd873630c282
OPSRv <opsrv1@gmail.com>
2022-01-03 05:58:57 +0200

    Update index.js

--- src/Services/api/axios/index.js
+++ src/Services/api/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://31.172.65.172/api/",
+  baseURL: "https://205366.fornex.cloud/api/",
 });
 
 axiosInstance.interceptors.request.use(
